eb1c1b5494c8b143650594fe21eedfc26de44186
PointerFLY <linghao.michael.ma@gmail.com>
2018-02-14 08:51:58 +0000

    Refract shader

--- Rendering-GLSL/transmitance-effects/shaders/refract_fs.glsl
+++ Rendering-GLSL/transmitance-effects/shaders/refract_fs.glsl
@@ -10,7 +10,8 @@ uniform samplerCube skybox;
 out vec4 fragColor;
 
 void main() {
+    float ratio = 1.00 / 1.52;
     vec3 surfaceToCamera = normalize(vPosition - cameraPosition);
-    vec3 reflectDir = reflect(surfaceToCamera, vNormal);
+    vec3 reflectDir = refract(surfaceToCamera, vNormal, ratio);
     fragColor = vec4(texture(skybox, reflectDir).rgb, 1.0);
 }
